1a7a1d3b0d86d8185295d1bece5977b9d60b5697
praveenkumar2007 <praveenk7020@gmail.com>
2025-09-02 07:34:42 -0700

    fatal:mycode

---
+++ praveen.js
@@ -0,0 +1,32 @@
+function hireme(){
+    var hireme=document.getElementById("hire-btn")
+    
+    var cont= confirm("do you Hire me")
+    if(true==cont){
+       alert("thank you")
+    }
+    else{
+    alert("thanks for visit my portfilio")
+    }
+}
+function letstalk(){
+    var talkme=document.getElementById("lets-btn")
+    
+    var msg= confirm("do you talk to me")
+    if(true==msg){
+       alert("i will contact you")
+    }
+    else{
+    alert("thanks for visit my portfilio")
+    }
+}
+function check(){
+    var detail=document.getElementById("sub-btn")
+    var check=confirm("I'am received your data will contact soon!")
+    if(check==true){
+        alert("THANKS FOR CONTACT ME")
+    }
+    else{
+        alert("thanks for visit my portfilio")
+    }
+}
